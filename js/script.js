0e9f4b228f6e9eb138f1d43edfcf1ef0627df769
Megan <hnmegan@gmail.com>
2019-07-09 17:33:16 -0400

    adding video and make it open and close properly

--- js/script.js
+++ js/script.js
@@ -5,21 +5,29 @@
 
 	const sigils = document.querySelectorAll('.sigilContainer'),
 		lightBox = document.querySelector('.lightbox'),
-		closeButton = document.querySelector('.close-lightbox');
+		closeButton = document.querySelector('.close-lightbox'),
+		houseVideo = document.querySelector('.house-video');
+
 
 	function popLightBox() {
 		// debug this so far and make sure the event handling works
 		//debugger;
 
 		lightBox.classList.add('show-lightbox');
+
+		houseVideo.play();
 	}
 
 	function closeLightBox(event) {
 		event.preventDefault();
 		//make the lightbox close
 	lightBox.classList.remove('show-lightbox');
+	houseVideo.currentTime = 0; // rewind the video
+	houseVideo.pause();
 	}
 
 	sigils.forEach(sigil => sigil.addEventListener("click", popLightBox));
 	closeButton.addEventListener("click", closeLightBox);
+
+	houseVideo.addEventListener("ended", closeLightBox);
 })();
